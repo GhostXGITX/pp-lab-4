051d7c6291da63e973292f0e12a0f42058347f65
Mikolaj <mikolaj.m.zulewski@gmail.com>
2024-04-14 11:45:21 +0200

    zad2

--- Company.java
+++ Company.java
@@ -1,13 +1,14 @@
+import employees.*;
 public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
         
-        employees[0] = new Employee("John Doe", 50000);
-        employees[1] = new Employee("Jane Smith", 60000);
+        employees[0] = new Manager("John Doe", 50000,0);
+        employees[1] = new Worker("Jane Smith", 60000,"Junior Developer");
         employees[2] = new Employee("Alice Johnson", 55000);
         employees[3] = new Employee("Bob Brown", 52000);
-        employees[4] = new Employee("Emma Wilson", 58000);
+        employees[4] = new Worker("Emma Wilson", 58000,"Team Leader");
 
        
         System.out.println("Dane dla pracownika o indeksie 3:");
